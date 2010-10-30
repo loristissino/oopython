4b8552311c4ef29841aee5107794b845445ee3c7
loris.tissino <loris.tissino@5be9ef65-fe4e-d553-c31d-209ad9341544>
2010-10-30 16:39:20 +0000

    added backup utility for knols

---
+++ utilities/processknols.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source processknols.rc
+TEMPDIR=$(mktemp -d)
+
+cd $TEMPDIR
+
+wget $SOURCE -O - | xpath -q -e '//div[@id="knol-collection-container"]//a[@class="knol-search-knol-title"]/@href' | sed -e 's/.*href="//' -e 's/"//' > urls
+
+
+NUMBER=0
+for url in $(cat urls); do
+	echo -n "Retrieving $url... "
+	NUMBER=$((NUMBER + 1))
+	FILENAME=$(printf "%03d.html" $NUMBER)
+	wget "$BASE$url" -O $FILENAME 2>/dev/null
+	echo "done."
+done
+
+zip $ZIPFILE *html
+
+rm -rf $TEMPDIR
+
