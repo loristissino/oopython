a7223e5d2035d9687cd4315e85ce85ced27567bb
loris.tissino <loris.tissino@5be9ef65-fe4e-d553-c31d-209ad9341544>
2010-11-02 15:25:55 +0000

    updated lesson 13 and backup file

---
+++ trunk/lessons/13/c/Fruit.py
@@ -0,0 +1,66 @@
+class Fruit(object):
+    __salad = 0
+
+    @classmethod
+    def __incSaladCount(cls):
+        cls.__salad += 1
+
+    @classmethod
+    def getSaladCount(cls):
+        return cls.__salad
+
+    __slots__ = ('name', '__alerts', '__quality')
+    
+    def __init__(self, name):
+        self.setName(name)
+        self.__alerts = []
+        self.__incSaladCount()
+
+    def __str__(self):
+        return self.getName()
+
+    def getName(self):
+        return self.name
+
+    def setName(self, value):
+        self.name = value
+        return self
+
+    def __addAlert(self, value):
+        self.__alerts.append(value)
+        return self
+
+    def getName(self):
+        return self.name
+
+    def getAlerts(self):
+        return self.__alerts
+
+    @property
+    def quality(self):
+        return self.__quality
+    
+    @quality.setter
+    def quality(self, value):
+        if 0 <= value <= 100: 
+            self.__quality = value
+        else:
+            self.__quality = 50
+            self.__addAlert('bad quality: %d' % value)
+
+    @property
+    def is_good(self):
+        return self.quality > 70
+
+def main():
+    myapple = Fruit('mela granny smith') 
+    myapple.quality = 10
+    print(myapple.quality, myapple.is_good)
+    myapple.quality = 75
+    print(myapple.quality, myapple.is_good)
+
+    # myapple.is_good = False
+    # non funziona (manca il setter)
+    
+if __name__ == '__main__':
+    main()
