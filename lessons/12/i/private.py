7d22589d1c5ec4c17f1a91b81968358210ef984a
loris.tissino <loris.tissino@5be9ef65-fe4e-d553-c31d-209ad9341544>
2010-11-03 08:46:29 +0000

    added lesson 12/i

---
+++ lessons/12/i/private.py
@@ -0,0 +1,14 @@
+class Foo():
+    def __init__(self):
+        self.bar = 1
+        self._bar = 2
+        self.__bar = 3
+
+f = Foo()
+print("f.bar: %d" % f.bar)
+print("f._bar: %d" % f._bar)
+#print("f.__bar: %d" % f.__bar)
+
+print(f.__dict__)
+print("f.__bar (accesso indiretto): %d" % f.__dict__['_Foo__bar'])
+
