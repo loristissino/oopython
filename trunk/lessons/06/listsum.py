f10f83bf9528f69b642eea00bf0127836ea45df0
loris.tissino <loris.tissino@5be9ef65-fe4e-d553-c31d-209ad9341544>
2010-12-04 15:16:52 +0000

    added lesson 6

---
+++ trunk/lessons/06/listsum.py
@@ -0,0 +1,15 @@
+import usefulfunctions
+
+def countdown(n, k=100, step=1):
+    '''restituisce una lista di *n* valori interi, decrementando a partire da *k*'''
+    l=[]
+    for i in range(n):
+        l.append(k-i*step)
+    return l
+        
+
+if __name__=='__main__':
+    n=countdown(10,250, 3)
+    print(n)
+    print('La somma è %d.' % sumup(n))
+    
