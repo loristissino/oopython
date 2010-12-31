980a7c0135999836093482b05f75eedd84794547
loris.tissino <loris.tissino@5be9ef65-fe4e-d553-c31d-209ad9341544>
2010-12-31 17:40:47 +0000

    updated lesson 9 examples

--- trunk/lessons/09/PictureBox.py
+++ trunk/lessons/09/PictureBox.py
@@ -3,7 +3,30 @@ from Picture import *
 class PictureBox(list):
     def showPictures(self):
         for i in range(len(self)):
-            print(self[i].getCompleteDescription())        
+            # NB len(self) ha senso in questo caso, visto che self è una lista
+            if isinstance(self[i], Picture):
+                # controlliamo, potrebbe essere None
+                print(self[i].getCompleteDescription())
+
+    def append(self, item):
+        # ridefinizione del metodo append (se si vuole essere sicuri che non
+        # si possano aggiungere elementi non di tipo Picture)
+        assert(isinstance(item, Picture))
+        super().append(item) # uso il metodo della classe padre
+
+    def insert(self, position, item):
+        # ridefinizione del metodo insert (se si vuole essere sicuri che non
+        # si possano aggiungere elementi non di tipo Picture)
+        assert(isinstance(item, Picture))
+        super().insert(position, item) # uso il metodo della classe padre       
+
+    def __setitem__(self, key, value):
+        # ridefinizione del metodo __setitem__ (se si vuole essere sicuri che non
+        # si possano cambiare elementi con oggetti non di tipo Picture, a meno
+        # che non li si imposti a None, che invece consideriamo cosa lecita)
+        assert(value is None or isinstance(value, Picture))
+        super().__setitem__(key, value) # uso il metodo della classe padre
+        
 
 if __name__=='__main__':
     mybox=PictureBox()
@@ -12,7 +35,7 @@ if __name__=='__main__':
 
     for i in range(n):
         pic=Picture()
-        pic.inputData()
-        mybox.append(pic)
+        pic.inputFields()
+        mybox.append(pic)   
 
     mybox.showPictures()
